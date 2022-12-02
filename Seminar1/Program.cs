be45b1202589dc2fd0ad93e5811b7b2a6057573a
Даниил Попереков <ivanptichev@gmail.com>
2022-12-02 10:56:05 +0400

    seminar1 added

---
+++ Seminar1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+Console.Write("Input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad = num2 * num2;
+if(num1 == quad)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+} 
+
+
+// задача 2 Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+/*
+Console.Write("Imput an integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = -num;
+
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current += 1;
+}
+*/
+
+//Задача 3 
