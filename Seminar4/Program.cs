e092036dff6047199b911c244e3d62aadfe35c23
Даниил Попереков <ivanptichev@gmail.com>
2022-12-09 12:44:29 +0400

    seminar4 added

---
+++ Seminar4/Program.cs
@@ -0,0 +1,96 @@
+﻿// Задача1: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+/*
+int NumOfDigits(int num)
+{
+    int count = 0;
+
+    while(num != 0)
+    {
+        count++;
+        num /= 10;
+    }
+    return count;
+}
+
+Console.Write("Imput a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = NumOfDigits(number);
+
+Console.WriteLine($" Number of digits in {number} is {result}");
+*/
+
+// Задача2: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А
+/*
+int SumNumber(int num)
+{
+   int sum = 0;
+
+    while( num > 0)
+    {
+        sum = sum + num;
+        num -= 1;
+    }
+    return sum;
+}
+
+Console.Write("Imput a num: ");
+Int num = Convert.ToInt32(Console.ReadLine());
+
+int result = SumNumber(number);
+
+Console.WriteLine($"{result}");
+*/
+
+
+// Задача3: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+/*
+int SumNumber(int num)
+{
+   int sum = 0;
+
+    while( num > 0)
+    {
+        sum = sum + num;
+        num -= 1;
+    }
+    return sum;
+}
+
+Console.Write("Imput a num: ");
+Int num = Convert.ToInt32(Console.ReadLine());
+
+int result = SumNumber(number);
+
+Console.WriteLine($"{result}");
+*/ // доделать
+
+
+// Задача4: Создать метод для создания массива.
+
+int[] CreateRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i < size; i++)
+         array[i] = new Random().Next(minValue, maxValue + 1);
+    return array;
+}
+
+void ShowArray( int[] array)
+{
+    for( int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"array [{i + 1}] is {array[i]}");
+    }
+}
+
+Console.Write("Input a number elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a min possible value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a max possible value ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateRandomArray(size, min, max);
+ShowArray(myArray);
